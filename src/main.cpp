aab619e2895579e5d599388f6f37b78af7010d1d
Vitaly <39009460+vitalybelo@users.noreply.github.com>
2023-01-10 15:56:48 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "nlohmann/json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
